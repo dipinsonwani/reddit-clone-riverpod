33b1477b1e01091ac0a2d7c6e198ef3bd4db3192
dipinsonwani <dipinsonwani@gmail.com>
2022-12-09 15:41:40 +0530

    Implement Login Screen

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,5 @@
+class Constants {
+  static const googleLogo = 'assets/images/google.png';
+  static const loginEmote = 'assets/images/loginEmote.png';
+  static const redditLogo = 'assets/images/logo.png';
+}
--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,12 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:reddit_clone_riverpod/core/common/sign_in_button.dart';
+import 'package:reddit_clone_riverpod/core/constants/constants.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          Constants.redditLogo,
+          height: 40,
+        ),
+        actions: [
+          TextButton(
+              onPressed: (() {}),
+              child: const Text(
+                'Skip',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ))
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          const SizedBox(
+            height: 30,
+          ),
+          const Text(
+            'Dive into anything',
+            style: TextStyle(
+                fontSize: 24, fontWeight: FontWeight.bold, letterSpacing: 0.5),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset(
+              Constants.loginEmote,
+              height: 400,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const SignInButton(),
+        ],
+      ),
+    );
   }
 }
